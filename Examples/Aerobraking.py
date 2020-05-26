12269232f13739d14eb6fc9d15900b054fa5e51c
Tristan Dijkstra <tristan.dijkstra@gmail.com>
2020-05-26 12:04:38 +0200

    Fixes to the aerobraking example

--- Examples/Aerobraking.py
+++ Examples/Aerobraking.py
@@ -5,7 +5,7 @@ import numpy as np
 
 DATAFILE = "runs/Aerobraking.csv"
 ATMOSPHEREDATA = "densityModels/MarsDensity.csv"
-SPEED = 1000  # __ times speed
+SPEED = 2000  # __ times speed
 RUNTIME = 300000
 print("Total runtime will be:", RUNTIME, "s or:", RUNTIME/3600, "hours or:", RUNTIME/86400, "days")
 
@@ -23,7 +23,7 @@ Mars = msp.Planet(4.282837e4, 3396.2, 1.52367934 * AU, muSun, MarsAtmosphere)
 r = np.array([21508.114845629447, 0.0, 982.3450283462487])
 v = np.array([-2.968111925169866, 0.0, -1.4808260236254678])
 
-# CD is increased times 50 here to see the effect.
+# CD is increased times 100 here to see the effect.
 CD = 1.23 * 100
 surfaceArea = 3.6**2 * math.pi
 
@@ -33,15 +33,15 @@ spacecraft.initPositionOrbit(r,v)
 # PROPAGATE Here
 dt = 1
 # These are some precalculated manoeuvres to see the effects
-spacecraft.addManoeuvreByDirection(spacecraft.start + 100, -1.332, "t")
-spacecraft.addManoeuvreByDirection(spacecraft.start + 8900, -0.3, "t")
+spacecraft.addManoeuvreByDirection(spacecraft.start + 100, -1.35, "t")
+spacecraft.addManoeuvreByDirection(spacecraft.start + 8900, -0.2, "t")
 
 rlist = spacecraft.propagate(RUNTIME, DATAFILE, True, dtAtmospheric = dt, dtNormal = dt)
 
-print(np.sqrt(spacecraft.v.dot(spacecraft.v)))
-print(spacecraft.e)
-print(spacecraft.periapsis)
-print("Periaosis alt", spacecraft.periapsis-Mars.r)
-print("Apoapsis alt", spacecraft.apoapsis-Mars.r)
+
+print(f"Final eccentricity {spacecraft.e}")
+print(f"Final velocity {np.sqrt(spacecraft.v.dot(spacecraft.v))}")
+print(f"Periapsis alt {spacecraft.periapsis-Mars.r}")
+print(f"Apoapsis alt {spacecraft.apoapsis-Mars.r}")
 
 simtools.quickAnimate(SPEED,DATAFILE,Mars)
